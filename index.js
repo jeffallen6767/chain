d96cceb05b9b4de465ede79dfe79cb299850f2aa
Jeff Allen <1063272+jeffallen6767@users.noreply.github.com>
2017-12-04 10:50:32 -0800

    auto-export all files in src dir

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+/* export all files in ./src dir */
 var 
-  hash = require("./src/hash"),
-  block = require("./src/block");
+  fs = require('fs'),
+  path = require('path'),
+  basePath = process.cwd(),
+  srcPath = path.resolve(basePath, 'src'),
+  files = fs.readdirSync(srcPath),
+  allFilesInSrcDir = files.reduce(function(acc, file, idx){
+    var
+      parts = file.split("."),
+      key = parts[0],
+      val = "./src/" + file;
+    acc[key] = require(val);
+    return acc;
+  }, {});
 
-module.exports = {
-  "hash": hash,
-  "block": block
-};
+module.exports = allFilesInSrcDir;

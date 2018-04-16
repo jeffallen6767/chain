e04cb797b46a87542aeed6241a6dbadb903fde20
Jeff Allen <1063272+jeffallen6767@users.noreply.github.com>
2018-04-16 06:05:38 -0700

    allow passing string to pull pre-made config from package.json

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
-var 
-  fs = require('fs'),
-  path = require('path'),
-  basePath = process.cwd(),
-  srcPath = path.resolve(basePath, 'src'),
-  files = fs.readdirSync(srcPath);
-
+// index.js
 module.exports = {
+  // callers need to supply an actual config object
+  // or pass a string like "dev", "qa", "test", or "production"
+  // to pull pre-made config from package.json
   "init": function(config) {
     var 
-      api = {};
-    config.path = basePath;
-    //console.log("-->index.js basePath", basePath);
+      fs = require('fs'),
+      path = require('path'),
+      api = {},
+      basePath = require.resolve("./").replace("index.js", ""),
+      srcPath = path.resolve(basePath, 'src'),
+      files = fs.readdirSync(srcPath),
+      finalConfig = typeof config === "string"
+        ? require(path.resolve(basePath, './package.json')).config[config]
+        : config;
+
+    finalConfig.path = basePath;
+    
     files.forEach(function(file) {
       var
         parts = file.split("."),
         key = parts[0],
         val = path.resolve(srcPath, file);
       if (fs.statSync(val).isFile()) {
-        //console.log("require and config FILE:", key, val);
-        api[key] = require(val).init(api, config);
+        api[key] = require(val).init(api, finalConfig);
       } else {
-        //console.log("skipping FOLDER:", key, val);
       }
     });
+    
     return api;
   }
 };
